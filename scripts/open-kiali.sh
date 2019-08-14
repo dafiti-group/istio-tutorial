62f46916e42710cfa9ad46fc655c3ba2ccdaddcc
CNCF Demo <demo@oracle.com>
2019-08-13 21:44:43 -0300

    Fix Kiali Address

--- scripts/open-kiali.sh
+++ scripts/open-kiali.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    gio open http://localhost:20001/console
+    gio open http://localhost:20001/kiali/
 elif [[ "$OSTYPE" == "darwin"* ]]; then 
-    open http://localhost:20001/console
+    open http://localhost:20001/kiali/
 fi
 
 kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=kiali -o jsonpath='{.items[0].metadata.name}') 20001:20001 &
